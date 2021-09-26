6f13daae41b41215a87376e2d76a24cdf96258ef
Richard Fu <fur@richardfu.net>
2021-09-26 23:45:59 +1000

    Init commit.

---
+++ Assets/RF Games/Editor/PrefabSceneConverter.cs
@@ -0,0 +1,109 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEditor.SceneManagement;
+
+public class PrefabSceneConverter : EditorWindow
+{
+	private const string PREFS_PREFAB_PATH = "PrefabSceneConverterPrefabPath";
+	private const string PREFS_SCENE_PATH = "PrefabSceneConverterScenePath";
+	private const string PREFS_OVERRIDE_EXIST = "PrefabSceneConverterOverrideExist";
+	private const string PREFS_ADD_TO_BUILD_SCENES = "PrefabSceneConverterAddToBuildScenes";
+
+	private string _prefabPath = "Prefabs/Levels/";
+	private string _scenePath = "Scenes/Levels/";
+	private string _prefabNames;
+	private bool _overrideExist = true;
+	private bool _addToBuildScenes = true;
+
+	private Vector2 _scroll;
+
+	[MenuItem("RF Games/Prefab Scene Converter")]
+	private static void Init()
+	{
+		PrefabSceneConverter window = (PrefabSceneConverter)GetWindow(typeof(PrefabSceneConverter));
+		window.Show();
+	}
+
+	private void Awake()
+	{
+		_prefabPath = EditorPrefs.GetString(PREFS_PREFAB_PATH, _prefabPath);
+		_scenePath = EditorPrefs.GetString(PREFS_SCENE_PATH, _scenePath);
+		_overrideExist = EditorPrefs.GetBool(PREFS_OVERRIDE_EXIST, _overrideExist);
+		_addToBuildScenes = EditorPrefs.GetBool(PREFS_ADD_TO_BUILD_SCENES, _addToBuildScenes);
+	}
+
+	private void OnGUI()
+	{
+		string prefabPath = EditorGUILayout.TextField("Prefab Path:", _prefabPath);
+		if (prefabPath != _prefabPath) {
+			EditorPrefs.SetString(PREFS_PREFAB_PATH, _prefabPath = prefabPath);
+		}
+
+		string scenePath = EditorGUILayout.TextField("Scene Path:", _scenePath);
+		if (scenePath != _scenePath) {
+			EditorPrefs.SetString(PREFS_SCENE_PATH, _scenePath = scenePath);
+		}
+
+		EditorGUILayout.LabelField("Prefab Names: (separate by new line)");
+
+		_scroll = EditorGUILayout.BeginScrollView(_scroll);
+		_prefabNames = EditorGUILayout.TextArea(_prefabNames, GUILayout.Height(position.height - 30));
+		EditorGUILayout.EndScrollView();
+
+		bool overrideExist = EditorGUILayout.Toggle("Override Exist:", _overrideExist);
+		if (overrideExist != _overrideExist) {
+			EditorPrefs.SetBool(PREFS_OVERRIDE_EXIST, _overrideExist = overrideExist);
+		}
+
+		bool addToBuildScenes = EditorGUILayout.Toggle("Add Scenes to Build Scenes", _addToBuildScenes);
+		if (addToBuildScenes != _addToBuildScenes) {
+			EditorPrefs.SetBool(PREFS_ADD_TO_BUILD_SCENES, _addToBuildScenes = addToBuildScenes);
+		}
+
+		if (GUILayout.Button("Convert")) {
+			string[] prefabNames = _prefabNames.Split('\n');
+			foreach (string prefabName in prefabNames) {
+
+				string newSceneAssetPath = Path.Combine("Assets/", _scenePath, prefabName + ".unity");
+				string newSceneFullPath = Path.Combine(Application.dataPath, _scenePath, prefabName + ".unity");
+
+				string prefabAssetPath = Path.Combine("Assets/", _prefabPath, prefabName + ".prefab");
+
+				bool newSceneExist = File.Exists(newSceneFullPath);
+
+				if (_overrideExist || !newSceneExist) {
+
+					Scene scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+					GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabAssetPath, typeof(GameObject));
+
+					if (prefab == null) {
+						Debug.LogError($"Prefab doesn't exist at {prefabAssetPath}, skipping...");
+						continue;
+					}
+
+					GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+					Debug.Log(go.name);
+
+					bool succeed = EditorSceneManager.SaveScene(scene, newSceneAssetPath);
+					Debug.Log($"Save scene {(succeed ? "succeed" : "failed")} at {newSceneAssetPath}");
+
+					if (succeed && _addToBuildScenes && !newSceneExist) {
+						EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+						EditorBuildSettingsScene[] newBuildScenes = new EditorBuildSettingsScene[buildScenes.Length + 1];
+						System.Array.Copy(buildScenes, newBuildScenes, buildScenes.Length);
+						EditorBuildSettingsScene newbuildScene = new EditorBuildSettingsScene(newSceneAssetPath, true);
+						newBuildScenes[newBuildScenes.Length - 1] = newbuildScene;
+						EditorBuildSettings.scenes = newBuildScenes;
+					}
+
+				} else {
+
+					Debug.Log($"Scene already exists at {newSceneAssetPath}, skipping...");
+
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Unity-PrefabSceneConverter
-A tool allows you to convert all level prefabs into scenes, so you can use them for addictive scene loading.
+> A tool allows you to convert all level prefabs into scenes, so you can use them for addictive scene loading.
+
+## Usage
+Open the tool from menu `RF Dev -> Prefab Scene Converter`. Simply put the names of all your prefabs into the textbox, click convert and it will search and convert all prefabs found in the given folder. It also provides an option to add the scene into the Build Scenes automatically (which you have to do for scene loading).
+
+Each prefab is simply put inside a scene with the same name, and becomes the only object in the corresponding scene. The original prefab is untouched so the level designer can still use the prefab for level design.
+
+![The screenshot of prefab scene converter](https://i0.wp.com/www.richardfu.net/wp-content/uploads/prefab-scene-converter.jpg?w=1282&ssl=1)
+
+Further reading for [Optimize Large Prefab Loading To Addictive Scene Loading with PrefabSceneConverter](https://www.richardfu.net/optimize-large-prefab-loading-to-addictive-scene-loading-with-prefabsceneconverter/), which explain why and when you will want to use this tool.
+
+## License
+Unity-PrefabSceneConverter is licensed under a MIT License.
